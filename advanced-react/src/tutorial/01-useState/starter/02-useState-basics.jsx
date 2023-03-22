0db7353ccde668eab15704c9ab064f6fc04694c9
Akash <akashkumarsingh11032001@gmail.com>
2023-03-22 21:20:23 +0530

    useState hooks

--- advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+// useState : ["default value","function to manipulate default value"]
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count+1);
+  }
+  return (
+    <>
+      <h2>current Count : {count}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
