b1d3e455515a24b96c67b95e68809e1231c7e8b4
Akash <akashkumarsingh11032001@gmail.com>
2023-03-22 21:07:21 +0530

    task accomplished

--- advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+
+  const OnIncrement = () => {
+    count = count + 1;
+    console.log(count);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button type="button" onClick={OnIncrement}>Increment</button>
+    </>
+  );
 };
 
 export default ErrorExample;
