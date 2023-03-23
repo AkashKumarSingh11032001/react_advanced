4a14f24073a2c003ee7f439de719b288e461c7ff
Akash <akashkumarsingh11032001@gmail.com>
2023-03-23 20:23:25 +0530

    add remove  person

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,6 @@
 // import Starter from "./tutorial/01-useState/starter/01-error-example";
-import Starter from "./tutorial/01-useState/starter/02-useState-basics";
+// import Starter from "./tutorial/01-useState/starter/02-useState-basics";
+import Starter from "./tutorial/01-useState/starter/03-useState-array.jsx";
 
 function App() {
   return (
--- advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,46 @@
+import { data } from "../../../data";
+import { useState } from "react";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeitem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h1>{name}</h1>
+            <button
+              type="button"
+              className="btn"
+              onClick={() => removeitem(id)}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        className="btn"
+        onClick={clearAllItems}
+        style={{ marginTop: "5rem", width: "10rem" }}
+      >
+        Clear All
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
